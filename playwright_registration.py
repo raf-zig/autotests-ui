1a95f686830f0189a9c44c5ee98962afa23d9a0c
raf-zig <ziravin@yandex.ru>
2025-06-27 23:24:12 +0500

    Введение в Playwrigth. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
