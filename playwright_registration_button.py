71c1d85af908e98e9294929784fc75be332e207a
raf-zig <ziravin@yandex.ru>
2025-06-30 20:49:23 +0500

    Углубление в Playwrigth. Практика по углубленной работе с Playwright

--- playwright_registration_button.py
+++ playwright_registration_button.py
@@ -5,6 +5,9 @@ with sync_playwright() as playwright:
     page = browser.new_page()
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_disabled()
+
     email_input = page.get_by_test_id('registration-form-email-input').locator('input')
     email_input.fill('user.name@gmail.com')
 
@@ -14,9 +17,4 @@ with sync_playwright() as playwright:
     password_input = page.get_by_test_id('registration-form-password-input').locator('input')
     password_input.fill('password')
 
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard).to_be_visible()
-    expect(dashboard).to_have_text("Dashboard")
+    expect(registration_button).not_to_be_disabled()
