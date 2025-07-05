bd3d543be75c59bc8e14dd0c91dabea64eedf4b2
raf-zig <ziravin@yandex.ru>
2025-07-06 01:31:46 +0500

    Знакомство с PageObject. Практика работы с Page Object

---
+++ tests/test_registration.py
@@ -0,0 +1,14 @@
+import time
+
+import pytest
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+
+@pytest.mark.regression
+@pytest.mark.authorization
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email="rav@mail.ru", username="rav", password="123"):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email, username, password)
+    registration_page.click_registration_button()
+
+    dashboard_page.check_visible_dashboard()
