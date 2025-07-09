1ed13f4fd0c704e9778ccd05f5bfb9f3a4ffc4bb
raf-zig <ziravin@yandex.ru>
2025-07-09 22:29:44 +0500

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,14 +1,13 @@
-import time
-
 import pytest
+
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email="rav@mail.ru", username="rav", password="123"):
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form(email, username, password)
+    registration_page.fill_registration_form("rav@mail.ru", "rav", "123")
     registration_page.click_registration_button()
 
     dashboard_page.check_visible_dashboard()
