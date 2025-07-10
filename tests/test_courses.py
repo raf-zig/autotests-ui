5a7cbb9782515f1c8b944247d6d40249fede0712
raf-zig <ziravin@yandex.ru>
2025-07-11 01:42:08 +0500

    Знакомство с PageComponent. Практика работы с Page Component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -3,32 +3,23 @@ import pytest
 from playwright.sync_api import expect, Page
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
-def test_empty_courses_list(chromium_page_with_state: Page):
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    course_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(course_title).to_be_visible()
-    expect(course_title).to_have_text("Courses")
+    courses_list_page.navbar.check_visible("username")
+    courses_list_page.sidebar.check_visible()
 
-    results_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
-    expect(results_icon).to_be_visible()
-
-    results_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(results_title).to_be_visible()
-    expect(results_title).to_have_text("There is no results")
-
-    results_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(results_text).to_be_visible()
-    expect(results_text).to_have_text("Results from the load test pipeline will be displayed here")
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_empty_view()
 
 @pytest.mark.courses
 @pytest.mark.regression
 def test_create_course(
-        chromium_page_with_state: Page,
         create_course_page: CreateCoursePage,
         courses_list_page: CoursesListPage
 ):
-    chromium_page_with_state.goto(
+    create_course_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
