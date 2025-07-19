36c472f4fe5eec96f1c549ed8ff20b402dce5ce4
raf-zig <ziravin@yandex.ru>
2025-07-20 00:13:49 +0500

    Добавление Allure шагов в автотесты. Практика работы с allure.step

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,11 +1,27 @@
 import pytest
+import allure
+from allure_commons.types import Severity
 
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.COURSES) # Добавили feature
+@allure.story(AllureStory.COURSES) # Добавили story
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self,courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
@@ -16,6 +32,8 @@ class TestCourses:
         courses_list_page.empty_view.check_visible(
             title='There is no results', description='Results from the load test pipeline will be displayed here')
 
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self,
             create_course_page: CreateCoursePage,
             courses_list_page: CoursesListPage):
@@ -52,6 +70,8 @@ class TestCourses:
             estimated_time="2 weeks"
         )
 
+    @allure.title("Edit course")
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(self,
             create_course_page: CreateCoursePage,
             courses_list_page: CoursesListPage):
