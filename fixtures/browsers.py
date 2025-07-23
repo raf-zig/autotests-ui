eb4456799c304c6ddbd7c912fe7889186d8ea7af
raf-zig <ziravin@yandex.ru>
2025-07-23 16:10:56 +0500

    Запуск автотестов на разных браузерах. Запускаем автотесты на разных браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -4,9 +4,12 @@ from _pytest.fixtures import SubRequest
 from pages.authentication.registration_page import RegistrationPage
 from config import settings
 from tools.playwright.pages import initialize_playwright_page
-@pytest.fixture
+from tools.routs import AppRoute
+
+
+@pytest.fixture(params=settings.browsers)
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, browser_type=request.param)
 
 
 @pytest.fixture(scope="session")
@@ -16,7 +19,7 @@ def initialize_browser_state(playwright: Playwright):
     page = context.new_page()
 
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.visit(AppRoute.REGISTRATION)
     registration_page.registration_form.fill(
         email=settings.test_user.email,
         username=settings.test_user.username,
@@ -28,10 +31,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
+@pytest.fixture(params=settings.browsers)
 def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Page:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
+        browser_type=request.param,
         storage_state=settings.browser_state_file
     )
